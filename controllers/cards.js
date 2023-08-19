c880de0b4ee63f4da2d431ab515d2521a4f87811
unknown <archeeandrews@yandex.ru>
2023-08-19 13:19:26 +0300

    fix: исправление ошибок после ревью №1

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,9 +15,9 @@ const deleteCard = (req, res) => {
     // eslint-disable-next-line consistent-return
     .catch((err) => {
       if (err.message === 'CastError') {
-        res.status(500).send({ message: 'Что-то пошло не так', err });
+        res.status(400).send({ message: 'Некорректный id карточки', err });
       } else {
-        return res.status(400).send({ message: 'Некорректный id карточки', err });
+        return res.status(500).send({ message: 'Что-то пошло не так', err });
       }
     });
 };
@@ -90,9 +90,9 @@ const dislikeCard = (req, res) => {
     // eslint-disable-next-line consistent-return
     .catch((err) => {
       if (err.message === 'CastError') {
-        res.status(500).send({ message: 'Что-то пошло не так', err });
+        res.status(400).send({ message: 'Некорректный id карточки', err });
       } else {
-        return res.status(400).send({ message: 'Некорректный id карточки', err });
+        return res.status(500).send({ message: 'Что-то пошло не так', err });
       }
     });
 };
