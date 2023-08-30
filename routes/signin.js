e74abfd877089689857917c8133217527df4f4aa
unknown <archeeandrews@yandex.ru>
2023-08-30 19:48:29 +0300

    проектная работа №14

--- routes/signin.js
+++ routes/signin.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
+const signinRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 
 const { login } = require('../controllers/users');
 
-router.post('/', celebrate({
+signinRouter.post('/', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
 }), login);
 
-module.exports = router;
+module.exports = signinRouter;
